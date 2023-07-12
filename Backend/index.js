768689078e64e9030e4fbbb2be7ea534fc6378b7
Amanmandal-M <amanmandal644@gmail.com>
2023-07-13 01:56:45 +0530

    Backend Code updates

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 app.use(cors())
 app.use(express.static('./tm-my-image-model'));
 
+app.get('/', (req,res)=>{
+  return res.send(`<h1 style="color:blue;text-align:center">Welcome in Teachable Machine Backend</h1>`)
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
